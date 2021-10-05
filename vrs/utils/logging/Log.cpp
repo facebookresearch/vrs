e4d7ff750d6c37935f561350dbbb4feb20110468
Yuta Inoue <yinoue@fb.com>
2021-10-05 16:13:21 -0700

    Add simpler version of logging/Log.h for open source
    Summary:
    logging library is the last dependency we have for us to open source core VRS module.
    Scott MacDonald is working on open sourcing xr logging so once that's open sourced we can simply switch to that.
    
    This diff stack adds a very simple logger, check, verify macro temporarily to make us move forward
    
    Reviewed By: georges-berenger
    
    Differential Revision: D31322153
    
    fbshipit-source-id: aca3167dcc90a1455fb13e703c089591bf9087cf

---
+++ vrs/utils/logging/Log.cpp
@@ -0,0 +1,35 @@
+// (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+
+#define DEFAULT_LOG_CHANNEL "Log"
+#include "Log.h"
+
+#include <fmt/color.h>
+
+namespace vrs::logging {
+
+void log(Level level, const char* channel, const std::string& message) {
+  fmt::color c;
+  std::string logLevel = "";
+  switch (level) {
+    case Level::Error:
+      c = fmt::color::red;
+      logLevel = "ERROR";
+      break;
+    case Level::Warning:
+      c = fmt::color::orange;
+      logLevel = "WARNING";
+      break;
+    case Level::Info:
+      c = fmt::color::green;
+      logLevel = "INFO";
+      break;
+    case Level::Debug:
+      c = fmt::color::yellow;
+      logLevel = "DEBUG";
+      break;
+    default:
+      c = fmt::color::white;
+  }
+  fmt::print(stderr, fg(c), "[{}][{}]: {}", channel, logLevel, message);
+}
+} // namespace vrs::logging
