489c8ae6fbdd6e96f831e7ebbb79aaf26fa4d095
Georges Berenger <gberenger@fb.com>
2021-10-13 13:18:47 -0700

    Implement vrs::os::getTimestampSec using chronos for oss
    Summary: For open sourcing, we need a vanilla implementation of getTimestampSec() that doesn't depend on visiontypes.
    
    Reviewed By: kiminoue7
    
    Differential Revision: D31601149
    
    fbshipit-source-id: da6cdd6b62816938cbc25530a3bc68ed4aeb926c

--- vrs/os/Time.cpp
+++ vrs/os/Time.cpp
@@ -1,15 +1,19 @@
 // Copyright (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
+#include <vrs/os/Time.h>
+
 #include <chrono>
 
-#include <visiontypes/clock/ProcessingClock.h>
-#include <vrs/os/Time.h>
+#include <vrs/os/Platform.h>
 
 namespace vrs::os {
 
+#if IS_VRS_OSS_CODE()
 double getTimestampSec() {
-  return visiontypes::ProcessingClock::now().asSeconds();
+  using namespace std::chrono;
+  return duration_cast<duration<double>>(steady_clock::now().time_since_epoch()).count();
 }
+#endif
 
 int64_t getCurrentTimeSecSinceEpoch() {
   using namespace std::chrono;
