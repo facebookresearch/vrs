866d4417eb50d1504b5439593722acf3e61ca663
Georges Berenger <gberenger@fb.com>
2021-11-11 00:30:53 -0800

    Make it all build on Ubuntu
    Summary:
    After trying to run our builds with cmake on Ubuntu, a few problems appeared.
    This diff addresses them...
    
    Reviewed By: kiminoue7
    
    Differential Revision: D32345188
    
    fbshipit-source-id: a0095f34ffef546314b0777be51722d811f5bc1f

--- cmake/LibrariesSetup.cmake
+++ cmake/LibrariesSetup.cmake
@@ -1,5 +1,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
+# We messed up the capitalization of FindxxHash.cmake, which we can't rename in place.
+# For now, let's put that file in a subfolder, until we can move it back here "later"...
+set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" "${VRS_SOURCE_DIR}/cmake/more")
+
 find_package(Boost REQUIRED
   COMPONENTS
     filesystem
--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,10 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
-set(OSS_BUILD_MODE ON CACHE BOOL "Force OSS build mode.")
-if ("${OSS_BUILD_MODE}")
-  message(STATUS "Forcing OSS build mode")
-endif()
-
 if ("${CMAKE_BUILD_TYPE}" STREQUAL "")
   set(CMAKE_BUILD_TYPE "Release" CACHE STRING
     "Type of build: Debug or Release." FORCE)
