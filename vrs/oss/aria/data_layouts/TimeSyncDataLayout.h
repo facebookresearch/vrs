0b9e05d288dcea650119468608a6103ea6b090f9
Elias Mueggler <eliasm@fb.com>
2021-11-22 13:13:31 -0800

    Add data layouts: time sync data layouts
    Summary:
    Adding data layout for time sync, no need to move stream ID as it is already public:
    https://www.internalfb.com/code/fbsource/[0fa9c2c13e4d]/arvr/libraries/vrs/vrs/StreamId.h?lines=110
    
    Reviewed By: georges-berenger
    
    Differential Revision: D32484558
    
    fbshipit-source-id: 27e88bed743fcd7e3f52262c07ef22ee972d3525

---
+++ vrs/oss/aria/data_layouts/TimeSyncDataLayout.h
@@ -0,0 +1,36 @@
+// Copyright (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+
+#pragma once
+
+#include <cstdint>
+
+#include <vrs/DataLayout.h>
+#include <vrs/DataPieces.h>
+
+namespace aria {
+
+struct TimeSyncConfigurationLayout : public vrs::AutoDataLayout {
+  static constexpr uint32_t kVersion = 1;
+
+  vrs::DataPieceValue<std::uint32_t> streamId{"stream_id"};
+
+  // Sample rate for time data [Hz]
+  vrs::DataPieceValue<double> sampleRateHz{"sample_rate_hz"};
+
+  vrs::AutoDataLayoutEnd endLayout;
+};
+
+struct TimeSyncDataLayout : public vrs::AutoDataLayout {
+  static constexpr uint32_t kVersion = 1;
+
+  // The capture timestamp in nanoseconds using a monotonic clock, same clock that
+  // is used for the VRS records timestamps
+  vrs::DataPieceValue<std::int64_t> monotonicTimestampNs{"monotonic_timestamp_ns"};
+
+  // The real time clock or wall clock in nanoseconds
+  vrs::DataPieceValue<std::int64_t> realTimestampNs{"real_timestamp_ns"};
+
+  vrs::AutoDataLayoutEnd endLayout;
+};
+
+} // namespace aria
