37261171e8587b5c36cccb796bf2a45bf622e298
Elias Mueggler <eliasm@fb.com>
2021-11-22 13:13:31 -0800

    Add data layouts: audio data layouts
    Summary:
    Adding data layout for audio. No need to move stream ID as it is already in the right file:
    https://www.internalfb.com/code/fbsource/[0fa9c2c13e4d]/arvr/libraries/vrs/vrs/StreamId.h?lines=82
    
    Differential Revision: D32484555
    
    fbshipit-source-id: 2f8b07289863b1ecee3acac336fcc39019a9ee84

---
+++ vrs/oss/aria/data_layouts/AudioDataLayout.h
@@ -0,0 +1,44 @@
+// Copyright (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+
+#pragma once
+
+#include <cstdint>
+
+#include <vrs/DataLayout.h>
+#include <vrs/DataLayoutConventions.h>
+#include <vrs/DataPieces.h>
+
+namespace aria {
+
+struct AudioConfigurationLayout : public vrs::AutoDataLayout {
+  static constexpr uint32_t kVersion = 2;
+
+  vrs::DataPieceValue<std::uint32_t> streamId{"stream_id"};
+
+  // Number of channels in the audio stream.
+  vrs::DataPieceValue<std::uint8_t> numChannels{vrs::DataLayoutConventions::kAudioChannelCount};
+
+  // Number of samples per second. Typical values: 44100Hz.
+  vrs::DataPieceValue<std::uint32_t> sampleRate{vrs::DataLayoutConventions::kAudioSampleRate};
+
+  // Format of each subsample, deciding the bits and type per subsample.
+  // Convertible from vrs::AudioSampleFormat.
+  vrs::DataPieceValue<std::uint8_t> sampleFormat{vrs::DataLayoutConventions::kAudioSampleFormat};
+
+  vrs::AutoDataLayoutEnd endLayout;
+};
+
+struct AudioDataLayout : public vrs::AutoDataLayout {
+  static constexpr uint32_t kVersion = 2;
+
+  // A list of timestamps of each sample in the block, following the same order they are stored in
+  // the ContentBlock.
+  vrs::DataPieceVector<std::int64_t> captureTimestampsNs{"capture_timestamps_ns"};
+
+  // Set 1 for muted, 0 otherwise.
+  vrs::DataPieceValue<std::uint8_t> audioMuted{"audio_muted"};
+
+  vrs::AutoDataLayoutEnd endLayout;
+};
+
+} // namespace aria
