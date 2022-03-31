c251f364868e4eb3566a4c28fb8f4bd8f542341a
Georges Berenger <gberenger@fb.com>
2022-03-31 01:48:47 -0700

    Add missing loggin macro
    Summary:
    A recent diff added the use of the rarely needed XR_LOGW_EVERY_N_SEC, which broke OSS builds.
    This diff is a quick fix until we can do a proper OSS implementation.
    
    Differential Revision: D35274969
    
    fbshipit-source-id: 6ac0778ff28f0be647e2c8cfa1f3a0289c02e57c

--- vrs/oss/logging/Log.h
+++ vrs/oss/logging/Log.h
@@ -32,14 +32,39 @@ enum class Level {
   Debug = 3,
 };
 
+/// Logging backend: redirect where you need, depending on the log level and your preferences.
 void log(Level level, const char* channel, const std::string& message);
+
+/// Logging backend: redirect where you need, depending on the log level and your preferences
+/// This variant filters out too frequent output, based on the call site and a minimum duration
+/// between printouts.
+/// Warning: Currently does the same thing as regular logging (no time filtering yet).
+void log_every_n_seconds(
+    const char* file,
+    int line,
+    Level level,
+    int nSeconds,
+    const char* channel,
+    const std::string& message);
 
 } // namespace logging
 } // namespace vrs
 
 #define XR_LOG_DEFAULT(level, ...) log(level, DEFAULT_LOG_CHANNEL, fmt::format(__VA_ARGS__))
+#define XR_LOG_EVERY_N_SEC_DEFAULT(level, nseconds, ...) \
+  log_every_n_seconds(                                   \
+      __FILE__, __LINE__, level, nseconds, DEFAULT_LOG_CHANNEL, fmt::format(__VA_ARGS__))
 
 #define XR_LOGD(...) XR_LOG_DEFAULT(vrs::logging::Level::Debug, __VA_ARGS__)
 #define XR_LOGI(...) XR_LOG_DEFAULT(vrs::logging::Level::Info, __VA_ARGS__)
 #define XR_LOGW(...) XR_LOG_DEFAULT(vrs::logging::Level::Warning, __VA_ARGS__)
 #define XR_LOGE(...) XR_LOG_DEFAULT(vrs::logging::Level::Error, __VA_ARGS__)
+
+#define XR_LOGD_EVERY_N_SEC(nseconds, ...) \
+  XR_LOG_EVERY_N_SEC_DEFAULT(vrs::logging::Level::Debug, nseconds, __VA_ARGS__)
+#define XR_LOGI_EVERY_N_SEC(nseconds, ...) \
+  XR_LOG_EVERY_N_SEC_DEFAULT(vrs::logging::Level::Info, nseconds, __VA_ARGS__)
+#define XR_LOGW_EVERY_N_SEC(nseconds, ...) \
+  XR_LOG_EVERY_N_SEC_DEFAULT(vrs::logging::Level::Warning, nseconds, __VA_ARGS__)
+#define XR_LOGE_EVERY_N_SEC(nseconds, ...) \
+  XR_LOG_EVERY_N_SEC_DEFAULT(vrs::logging::Level::Error, nseconds, __VA_ARGS__)
