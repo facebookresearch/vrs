11dc594d4193738ca8b8af6a64856fd62eb26acc
Georges Berenger <gberenger@fb.com>
2022-04-13 14:56:39 -0700

    Split AudioExtractor
    Summary:
    The audio extraction code needs to be split with the cli option in vrs/utils/cli, which the audio extraction code stays in vrs/utils, so all-extraction can use audio extraction, too.
    This is nearly only moving code around.
    
    Reviewed By: SeaOtocinclus
    
    Differential Revision: D35612938
    
    fbshipit-source-id: b67f9558adf30f27b4f815bef6a6ff8ccdfca404

--- vrs/utils/AudioExtractor.h
+++ vrs/utils/AudioExtractor.h
@@ -12,15 +12,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <vrs/utils/FilteredFileReader.h>
+#pragma once
+
+#include <fstream>
+
+#include <vrs/RecordFormatStreamPlayer.h>
 
 namespace vrs::utils {
 
-/// Helper to extract raw audio streams from a VRS file into WAV files.
-/// Streams containing no audio will be ignored.
-/// @param folderPath: path to a folder where to extract the files
-/// @param filteredReader: filtered reader for the file to read from.
-/// @return A status code, 0 meaning success.
-int extractAudio(const std::string& folderPath, FilteredFileReader& filteredReader);
+class AudioExtractor : public RecordFormatStreamPlayer {
+ public:
+  AudioExtractor(const string& folderPath, StreamId id, uint32_t& counter);
+
+  ~AudioExtractor() override;
+
+  bool onAudioRead(const CurrentRecord& record, size_t, const ContentBlock& audioBlock) override;
+  bool onUnsupportedBlock(const CurrentRecord& record, size_t, const ContentBlock& cb) override;
+
+ protected:
+  // folder to save wav files to
+  string folderPath_;
+  // device id & instance for the stream we are operating on
+  StreamId id_;
+  // used to sum up the total number of audio files written out across all
+  // streams (caller provides a reference so this class can add to it)
+  uint32_t& cumulativeOutputAudioFileCount_;
+  // count of audio files written out in this specific stream
+  uint32_t streamOutputAudioFileCount_ = 0;
+  // used to track compatibility of successive audio blocks within a stream;
+  // if format changes, we close the wav file and start a new one
+  AudioContentBlockSpec currentAudioContentBlockSpec_;
+  // used to track total size of wav data written to file, so the .wav header
+  // can be properly updated before being finalized on disk
+  uint32_t totalAudioDataSize_ = 0;
+  // output stream of wav file currently being written
+  std::ofstream currentWavFile_;
+  // temp audio buffer to hold segment of audio to be written to file
+  vector<uint8_t> audio_;
+  // For validation: start timestamp of the audio segment
+  double segmentStartTimestamp_ = 0;
+  // For validation: count of audio samples previously processed since the start of the segment
+  uint64_t segmentSamplesCount_ = 0;
+};
 
 } // namespace vrs::utils
