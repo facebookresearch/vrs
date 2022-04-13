700dce019fa3848fa29b90647c7401ef4c4806bf
George Saltzman <georges@fb.com>
2022-04-13 08:31:55 -0700

    Split AudioExtractor
    Summary:
    The audio extraction code needs to be split with the cli option in vrs/utils/cli, which the audio extraction code stays in vrs/utils, so all-extraction can use audio extraction, too.
    This is nearly only moving code around.
    
    Differential Revision: D35612938
    
    fbshipit-source-id: 396c40fe9ecddcccb863ef77d49f456fe1b4e9d3

--- vrs/utils/AudioExtractor.cpp
+++ vrs/utils/AudioExtractor.cpp
@@ -15,14 +15,14 @@
 #include "AudioExtractor.h"
 
 #include <array>
-#include <fstream>
+#include <iostream>
 
 #define DEFAULT_LOG_CHANNEL "AudioExtractor"
 #include <logging/Log.h>
 #include <logging/Verify.h>
-#include <vrs/helpers/Endian.h>
 
-#include <vrs/RecordFormatStreamPlayer.h>
+#include <vrs/RecordReaders.h>
+#include <vrs/helpers/Endian.h>
 #include <vrs/os/Utils.h>
 
 using namespace std;
@@ -31,7 +31,7 @@ using namespace vrs;
 namespace {
 
 template <class T>
-static void writeHeader(void* p, const T t) {
+void writeHeader(void* p, const T t) {
   memcpy(p, &t, sizeof(T));
 }
 
@@ -42,6 +42,7 @@ void startNewWavFile(
     StreamId id,
     uint32_t audioCounter,
     double timestamp) {
+  XR_VERIFY(os::makeDirectories(folderPath) == 0);
   string path = fmt::format(
       "{}/{}-{:04}-{:.3f}.wav", folderPath, id.getNumericName(), audioCounter, timestamp);
   cout << "Writing " << path << endl;
@@ -103,7 +104,7 @@ void startNewWavFile(
   outFile.write((char*)fileHeader.data(), fileHeader.size());
 }
 
-static void closeExistingWavFile(ofstream& outFile, uint32_t totalAudioDataSize) {
+void closeExistingWavFile(ofstream& outFile, uint32_t totalAudioDataSize) {
   // close existing file if there is one
   if (outFile.is_open()) {
     // seek to beginning, update chunk & file sizes in header before closing
@@ -117,7 +118,7 @@ static void closeExistingWavFile(ofstream& outFile, uint32_t totalAudioDataSize)
   }
 }
 
-static uint32_t writeAudioBlock(
+uint32_t writeAudioBlock(
     ofstream& outFile,
     const AudioContentBlockSpec& audioBlock,
     const vector<uint8_t>& audio) {
@@ -141,145 +142,94 @@ static uint32_t writeAudioBlock(
   return bytesWritten;
 }
 
-class RecordFormatAudioSaver : public RecordFormatStreamPlayer {
- public:
-  RecordFormatAudioSaver(const string& folderPath, StreamId id, uint32_t& counter)
-      : folderPath_{folderPath},
-        id_{id},
-        cumulativeOutputAudioFileCount_{counter},
-        currentAudioContentBlockSpec_{AudioFormat::UNDEFINED} {}
-
-  ~RecordFormatAudioSaver() override {
-    // Need to write header size before closing file
-    closeExistingWavFile(currentWavFile_, totalAudioDataSize_);
-  }
-
-  bool onAudioRead(const CurrentRecord& record, size_t, const ContentBlock& audioBlock) override {
-    audio_.resize(audioBlock.getBlockSize());
-    int readStatus = record.reader->read(audio_);
-    if (readStatus != 0) {
-      XR_LOGW(
-          "{} - {} record @ {}: Failed read audio data ({}).",
-          record.streamId.getNumericName(),
-          toString(record.recordType),
-          record.timestamp,
-          errorCodeToMessage(readStatus));
-      return false;
-    }
-
-    const AudioContentBlockSpec& audioBlockSpec = audioBlock.audio();
-    if (audioBlockSpec.getAudioFormat() != AudioFormat::PCM) {
-      cout << "Skipping non-PCM audio block\n";
-      return true;
-    }
-
-    if (!currentAudioContentBlockSpec_.isCompatibleWith(audioBlockSpec)) {
-      closeExistingWavFile(currentWavFile_, totalAudioDataSize_);
-      totalAudioDataSize_ = 0;
-      startNewWavFile(
-          currentWavFile_,
-          audioBlockSpec,
-          folderPath_,
-          id_,
-          streamOutputAudioFileCount_,
-          record.timestamp);
-      currentAudioContentBlockSpec_ = audioBlockSpec;
-      cumulativeOutputAudioFileCount_++;
-      streamOutputAudioFileCount_++;
-      segmentStartTimestamp_ = record.timestamp;
-      segmentSamplesCount_ = 0;
-    }
-
-    totalAudioDataSize_ += writeAudioBlock(currentWavFile_, audioBlockSpec, audio_);
-
-    // Time/sample counting validation
-    if (segmentSamplesCount_ > 0) {
-      const double kMaxJitter = 0.01;
-      double actualTime = record.timestamp - segmentStartTimestamp_;
-      double expectedTime =
-          static_cast<double>(segmentSamplesCount_) / currentAudioContentBlockSpec_.getSampleRate();
-      if (actualTime - expectedTime > kMaxJitter) {
-        XR_LOGW("Audio block at {:.3f} {:.3f} ms late.", actualTime, actualTime - expectedTime);
-      } else if (expectedTime - actualTime > kMaxJitter) {
-        XR_LOGW(
-            "Audio block at {:.3f} {:.3f} ms, {:.2f}% early.",
-            expectedTime,
-            expectedTime - actualTime,
-            (1. - actualTime / expectedTime) * 100);
-      }
-    } else {
-      segmentStartTimestamp_ = record.timestamp;
-    }
-    segmentSamplesCount_ += audioBlock.audio().getSampleCount();
-
-    return true; // read next blocks, if any
-  }
-  bool onUnsupportedBlock(const CurrentRecord& record, size_t, const ContentBlock& cb) override {
-    // the audio was not decoded ... not sure why?
-    if (cb.getContentType() == ContentType::AUDIO) {
-      XR_LOGW("Audio block skipped for {}, content: {}", record.streamId.getName(), cb.asString());
-    }
-    return false;
-  }
-
- protected:
-  // folder to save wav files to
-  string folderPath_;
-  // device id & instance for the stream we are operating on
-  StreamId id_;
-  // used to sum up the total number of audio files written out across all
-  // streams (caller provides a reference so this class can add to it)
-  uint32_t& cumulativeOutputAudioFileCount_;
-  // count of audio files written out in this specific stream
-  uint32_t streamOutputAudioFileCount_ = 0;
-  // used to track compatibility of successive audio blocks within a stream;
-  // if format changes, we close the wav file and start a new one
-  AudioContentBlockSpec currentAudioContentBlockSpec_;
-  // used to track total size of wav data written to file, so the .wav header
-  // can be properly updated before being finalized on disk
-  uint32_t totalAudioDataSize_ = 0;
-  // output stream of wav file currently being written
-  ofstream currentWavFile_;
-  // temp audio buffer to hold segment of audio to be written to file
-  vector<uint8_t> audio_;
-  // For validation: start timestamp of the audio segment
-  double segmentStartTimestamp_ = 0;
-  // For validation: count of audio samples previously processed since the start of the segment
-  uint64_t segmentSamplesCount_ = 0;
-};
-
 } // namespace
 
 namespace vrs::utils {
 
-int extractAudio(const string& path, FilteredFileReader& filteredReader) {
-  if (path.length() > 0) {
-    if (!os::pathExists(path)) {
-      int status = os::makeDirectories(path);
-      if (status != 0) {
-        XR_LOGE("Can't create output directory at {}", path);
-        return status;
-      }
-    }
-    if (!os::isDir(path)) {
-      XR_LOGE("Can't write output files at {}, because something is there...", path);
-      return FAILURE;
-    }
+AudioExtractor::AudioExtractor(const string& folderPath, StreamId id, uint32_t& counter)
+    : folderPath_{folderPath},
+      id_{id},
+      cumulativeOutputAudioFileCount_{counter},
+      currentAudioContentBlockSpec_{AudioFormat::UNDEFINED} {}
+
+AudioExtractor::~AudioExtractor() {
+  // Need to write header size before closing file
+  closeExistingWavFile(currentWavFile_, totalAudioDataSize_);
+}
+
+bool AudioExtractor::onAudioRead(
+    const CurrentRecord& record,
+    size_t,
+    const ContentBlock& audioBlock) {
+  audio_.resize(audioBlock.getBlockSize());
+  int readStatus = record.reader->read(audio_);
+  if (readStatus != 0) {
+    XR_LOGW(
+        "{} - {} record @ {}: Failed read audio data ({}).",
+        record.streamId.getNumericName(),
+        toString(record.recordType),
+        record.timestamp,
+        errorCodeToMessage(readStatus));
+    return false;
   }
-  uint32_t audioFileCount = 0;
-  uint32_t streamCount = 0;
-  deque<unique_ptr<StreamPlayer>> extractors;
-  for (auto id : filteredReader.filter.streams) {
-    if (filteredReader.reader.mightContainAudio(id)) {
-      extractors.emplace_back(move(new RecordFormatAudioSaver(path, id, audioFileCount)));
-      filteredReader.reader.setStreamPlayer(id, extractors.back().get());
-      ++streamCount;
-    }
+
+  const AudioContentBlockSpec& audioBlockSpec = audioBlock.audio();
+  if (audioBlockSpec.getAudioFormat() != AudioFormat::PCM) {
+    cout << "Skipping non-PCM audio block\n";
+    return true;
   }
-  filteredReader.iterateSafe();
-  cout << "Wrote " << audioFileCount << " audio file(s) from " << streamCount << " stream(s)."
-       << endl;
-  return 0;
+
+  if (!currentAudioContentBlockSpec_.isCompatibleWith(audioBlockSpec)) {
+    closeExistingWavFile(currentWavFile_, totalAudioDataSize_);
+    totalAudioDataSize_ = 0;
+    startNewWavFile(
+        currentWavFile_,
+        audioBlockSpec,
+        folderPath_,
+        id_,
+        streamOutputAudioFileCount_,
+        record.timestamp);
+    currentAudioContentBlockSpec_ = audioBlockSpec;
+    cumulativeOutputAudioFileCount_++;
+    streamOutputAudioFileCount_++;
+    segmentStartTimestamp_ = record.timestamp;
+    segmentSamplesCount_ = 0;
+  }
+
+  totalAudioDataSize_ += writeAudioBlock(currentWavFile_, audioBlockSpec, audio_);
+
+  // Time/sample counting validation
+  if (segmentSamplesCount_ > 0) {
+    const double kMaxJitter = 0.01;
+    double actualTime = record.timestamp - segmentStartTimestamp_;
+    double expectedTime =
+        static_cast<double>(segmentSamplesCount_) / currentAudioContentBlockSpec_.getSampleRate();
+    if (actualTime - expectedTime > kMaxJitter) {
+      XR_LOGW("Audio block at {:.3f} {:.3f} ms late.", actualTime, actualTime - expectedTime);
+    } else if (expectedTime - actualTime > kMaxJitter) {
+      XR_LOGW(
+          "Audio block at {:.3f} {:.3f} ms, {:.2f}% early.",
+          expectedTime,
+          expectedTime - actualTime,
+          (1. - actualTime / expectedTime) * 100);
+    }
+  } else {
+    segmentStartTimestamp_ = record.timestamp;
+  }
+  segmentSamplesCount_ += audioBlock.audio().getSampleCount();
+
+  return true; // read next blocks, if any
+}
+
+bool AudioExtractor::onUnsupportedBlock(
+    const CurrentRecord& record,
+    size_t,
+    const ContentBlock& cb) {
+  // the audio was not decoded ... not sure why?
+  if (cb.getContentType() == ContentType::AUDIO) {
+    XR_LOGW("Audio block skipped for {}, content: {}", record.streamId.getName(), cb.asString());
+  }
+  return false;
 }
 
 } // namespace vrs::utils
