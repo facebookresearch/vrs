ce06e343b3c010c2258ff09e7e05a3a1c7a655af
Pierre Moulon <pierrem@fb.com>
2022-05-25 13:29:26 -0700

    Add Github Actions CI for MacOS (#49)
    Summary: Pull Request resolved: https://github.com/facebookresearch/vrs/pull/49
    
    Differential Revision: D36668840
    
    fbshipit-source-id: c37ac5566138e70d13dbb283bf5710d855e37e69

--- cmake/FindFmt.cmake
+++ cmake/FindFmt.cmake
@@ -12,4 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-find_package(fmt REQUIRED)
+find_package(fmt QUIET)
+
+if (NOT TARGET fmt::fmt)
+  find_path(Fmt_INCLUDE_DIR fmt/core.h)
+  mark_as_advanced(Fmt_INCLUDE_DIR)
+
+  find_library(Fmt_LIBRARY NAMES fmt fmtd)
+  mark_as_advanced(Fmt_LIBRARY)
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(
+    fmt
+    DEFAULT_MSG
+    Fmt_LIBRARY Fmt_INCLUDE_DIR
+  )
+  set(Fmt_LIBRARIES ${Fmt_LIBRARY})
+  set(Fmt_INCLUDE_DIRS ${Fmt_INCLUDE_DIR})
+  add_library(fmt::fmt UNKNOWN IMPORTED)
+  set_target_properties(
+    fmt::fmt PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${Fmt_INCLUDE_DIR}"
+  )
+  set_target_properties(
+    fmt::fmt PROPERTIES
+    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+    IMPORTED_LOCATION "${Fmt_LIBRARY}"
+  )
+endif()
