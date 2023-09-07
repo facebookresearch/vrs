0c1f7c2daf049b71a18e7f839d9169e00db7701f
Matthew Ahn <mahn@meta.com>
2023-09-06 17:44:58 -0700

    Fix enum-enum conversion issue in vrsplayer (#134)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/vrs/pull/134
    
    static_cast<int>(enum) before bit wise or. This should resolve the -Wdeprecated-enum-enum-conversion warning and thus won't need to ignore this warning in this library.
    
    Fixing this will also ensure build-calibration-cpp20 will pass.
    
    ```
    buck build arvr/mode/platform010/c++20/opt-stripped //arvr/libraries/vrs/oss/tools/vrsplayer:vrsplayerlib
    arvr/libraries/vrs/oss/tools/vrsplayer/PlayerWindow.cpp:91:36: error: bitwise operation between different enumeration types ('Qt::Modifier' and 'Qt::Key') is deprecated [-Werror,-Wdeprecated-enum-enum-conversion]
      saveFrames->setShortcut(Qt::CTRL | Qt::Key_S);
                              ~~~~~~~~ ^ ~~~~~~~~~
    arvr/libraries/vrs/oss/tools/vrsplayer/PlayerWindow.cpp:146:42: error: bitwise operation between different enumeration types ('Qt::Modifier' and 'Qt::Key') is deprecated [-Werror,-Wdeprecated-enum-enum-conversion]
          recallAction->setShortcut(Qt::CTRL | static_cast<Qt::Key>(Qt::Key_0 + number));
                                    ~~~~~~~~ ^ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    arvr/libraries/vrs/oss/tools/vrsplayer/PlayerWindow.cpp:200:37: error: bitwise operation between different enumeration types ('Qt::Modifier' and 'Qt::Key') is deprecated [-Werror,-Wdeprecated-enum-enum-conversion]
      smallerFont->setShortcut(Qt::CTRL | Qt::Key_Minus);
                               ~~~~~~~~ ^ ~~~~~~~~~~~~~
    arvr/libraries/vrs/oss/tools/vrsplayer/PlayerWindow.cpp:204:37: error: bitwise operation between different enumeration types ('Qt::Modifier' and 'Qt::Key') is deprecated [-Werror,-Wdeprecated-enum-enum-conversion]
      largestFont->setShortcut(Qt::CTRL | Qt::Key_Plus);
                               ~~~~~~~~ ^ ~~~~~~~~~~~~
    arvr/libraries/vrs/oss/tools/vrsplayer/PlayerWindow.cpp:215:41: error: bitwise operation between different enumeration types ('Qt::Modifier' and 'Qt::Key') is deprecated [-Werror,-Wdeprecated-enum-enum-conversion]
      solidBackground->setShortcut(Qt::CTRL | Qt::Key_B);
                                   ~~~~~~~~ ^ ~~~~~~~~~
    ```
    
    Reviewed By: jhouben-fb
    
    Differential Revision: D49025282
    
    fbshipit-source-id: dea916a8d40ee4218448188a09fed43558156d21

--- tools/vrsplayer/PlayerWindow.cpp
+++ tools/vrsplayer/PlayerWindow.cpp
@@ -81,14 +81,15 @@ void PlayerWindow::createMenus() {
   fileMenu_->addAction(openAction);
 
   QAction* openPathOrUriAction = new QAction("Open Path or URI...", this);
-  openPathOrUriAction->setShortcut(Qt::CTRL | Qt::SHIFT | Qt::Key_O);
+  openPathOrUriAction->setShortcut(
+      static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::SHIFT) | static_cast<int>(Qt::Key_O));
   openPathOrUriAction->setStatusTip("Open a recording using a path or URI...");
   connect(openPathOrUriAction, &QAction::triggered, &player_, &vrsp::PlayerUI::openPathChooser);
   fileMenu_->addAction(openPathOrUriAction);
 
   fileMenu_->addSeparator();
   QAction* saveFrames = new QAction("Save All Frames to...", this);
-  saveFrames->setShortcut(Qt::CTRL | Qt::Key_S);
+  saveFrames->setShortcut(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::Key_S));
   saveFrames->setStatusTip("Save Visible Frames to Folder...");
   connect(saveFrames, &QAction::triggered, &player_, &vrsp::PlayerUI::saveFrames);
   fileMenu_->addAction(saveFrames);
@@ -143,7 +144,7 @@ void PlayerWindow::updateLayoutMenu(
       recallAction = make_unique<QAction>(QString("Recall Preset '" + key + "'"), this);
     }
     if (++number < 10) {
-      recallAction->setShortcut(Qt::CTRL | static_cast<Qt::Key>(Qt::Key_0 + number));
+      recallAction->setShortcut(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::Key_0 + number));
     }
     connect(recallAction.get(), &QAction::triggered, [this, key]() { player_.recallPreset(key); });
     layoutMenu_->addAction(recallAction.get());
@@ -197,11 +198,11 @@ void PlayerWindow::updateTextOverlayMenu() {
   addColorAction(color, Qt::magenta, "Use Magenta");
   textOverlayMenu_->addSeparator();
   QAction* smallerFont = new QAction("Smaller Font", this);
-  smallerFont->setShortcut(Qt::CTRL | Qt::Key_Minus);
+  smallerFont->setShortcut(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::Key_Minus));
   connect(smallerFont, &QAction::triggered, [this]() { player_.adjustOverlayFontSize(-1); });
   textOverlayMenu_->addAction(smallerFont);
   QAction* largestFont = new QAction("Larger Font", this);
-  largestFont->setShortcut(Qt::CTRL | Qt::Key_Plus);
+  largestFont->setShortcut(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::Key_Plus));
   connect(largestFont, &QAction::triggered, [this]() { player_.adjustOverlayFontSize(+1); });
   textOverlayMenu_->addAction(largestFont);
   textOverlayMenu_->addSeparator();
@@ -212,7 +213,7 @@ void PlayerWindow::updateTextOverlayMenu() {
   });
   solidBackground->setCheckable(true);
   solidBackground->setChecked(isSolid);
-  solidBackground->setShortcut(Qt::CTRL | Qt::Key_B);
+  solidBackground->setShortcut(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::Key_B));
   textOverlayMenu_->addAction(solidBackground);
 }
 
