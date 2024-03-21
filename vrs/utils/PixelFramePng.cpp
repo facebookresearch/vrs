3b1b8e443f98921c189848c81464c1371978631d
Georges Berenger <gberenger@meta.com>
2024-03-21 09:13:54 -0700

    Use wuffs if possible to decode png images
    Summary:
    Wuffs can be significantly faster when decoding png images. Let's add support for it.
    Caveat: not all pixel formats are supported, so we fallback to original implementation when wuffs fails.
    
    Reviewed By: una-dinosauria
    
    Differential Revision: D55116440
    
    fbshipit-source-id: 225ca9775d721a32c336a773e31474a2889695ff

--- vrs/utils/PixelFramePng.cpp
+++ vrs/utils/PixelFramePng.cpp
@@ -28,6 +28,21 @@
 #include <vrs/helpers/MemBuffer.h>
 #include <vrs/os/Utils.h>
 
+#ifdef USE_WUFFS
+
+#define WUFFS_IMPLEMENTATION
+#define WUFFS_CONFIG__STATIC_FUNCTIONS
+#define WUFFS_CONFIG__MODULES
+#define WUFFS_CONFIG__MODULE__ADLER32
+#define WUFFS_CONFIG__MODULE__BASE
+#define WUFFS_CONFIG__MODULE__CRC32
+#define WUFFS_CONFIG__MODULE__DEFLATE
+#define WUFFS_CONFIG__MODULE__PNG
+#define WUFFS_CONFIG__MODULE__ZLIB
+#include <wuffs-v0.3.c>
+
+#endif
+
 namespace {
 constexpr size_t kPngSigBytes = 8;
 }
@@ -36,21 +51,214 @@ namespace vrs::utils {
 
 using namespace std;
 
+#ifdef USE_WUFFS
+
+static bool readPngFrameWithWuff(PixelFrame& frame, const vector<uint8_t>& pngBuffer) {
+  wuffs_base__io_buffer src =
+      wuffs_base__ptr_u8__reader(const_cast<uint8_t*>(pngBuffer.data()), pngBuffer.size(), true);
+
+  wuffs_png__decoder dec;
+  memset((void*)&dec, 0, sizeof(wuffs_png__decoder));
+  if (wuffs_png__decoder__initialize(
+          &dec, sizeof(wuffs_png__decoder), WUFFS_VERSION, WUFFS_INITIALIZE__ALREADY_ZEROED)
+          .repr) {
+    XR_LOGE("Failed to initialize wuffs png decoder!");
+    return false;
+  }
+
+  wuffs_base__image_config ic;
+  wuffs_base__status status = wuffs_png__decoder__decode_image_config(&dec, &ic, &src);
+  if (status.repr) {
+    XR_LOGE("Invalid png!");
+    return false;
+  }
+
+  uint32_t dim_x = wuffs_base__pixel_config__width(&ic.pixcfg);
+  uint32_t dim_y = wuffs_base__pixel_config__height(&ic.pixcfg);
+  size_t num_pixels = dim_x * dim_y;
+  if (num_pixels > (SIZE_MAX / 4)) {
+    XR_LOGE("Too large png!");
+    return false;
+  }
+  auto pixel_format = wuffs_base__pixel_config__pixel_format(&ic.pixcfg);
+
+  if (!wuffs_base__pixel_format__is_interleaved(&pixel_format)) {
+    // XR_LOGE("PNG must be interleaved!");
+    return false;
+  }
+
+  int bpp = wuffs_base__pixel_format__bits_per_pixel(&pixel_format);
+  int channels = 0;
+  uint32_t pixfmt_repr = 0;
+  switch (pixel_format.repr) {
+    case WUFFS_BASE__PIXEL_FORMAT__A:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__A;
+      channels = 1;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__Y:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__Y;
+      channels = 1;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__INDEXED__BGRA_PREMUL:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_PREMUL;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__INDEXED__BGRA_BINARY:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_BINARY;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__BGR_565:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGB;
+      channels = 3;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__BGR:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGB;
+      channels = 3;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__BGRA_NONPREMUL:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_NONPREMUL;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__BGRA_NONPREMUL_4X16LE:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_NONPREMUL;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__BGRA_PREMUL:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_PREMUL;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__BGRA_PREMUL_4X16LE:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_PREMUL;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__BGRA_BINARY:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_BINARY;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__BGRX:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBX;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__RGB:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGB;
+      channels = 3;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__RGBA_NONPREMUL:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_NONPREMUL;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__RGBA_NONPREMUL_4X16LE:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_NONPREMUL;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__RGBA_PREMUL:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_PREMUL;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__RGBA_PREMUL_4X16LE:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_PREMUL;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__RGBA_BINARY:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_BINARY;
+      channels = 4;
+      break;
+    case WUFFS_BASE__PIXEL_FORMAT__RGBX:
+      pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBX;
+      channels = 4;
+      break;
+
+    default: {
+      if (bpp % 8 != 0) {
+        // XR_LOGE("Wrong bit ber pixel is not divisible by 8 (12 bit PNG?)");
+        return false;
+      }
+
+      // TODO: figure out more reliable way to get channel count from wuffs
+      channels = bpp / 8;
+      switch (channels) {
+        case 1:
+          pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__A;
+          break;
+        case 3:
+          pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGB;
+          break;
+        case 4:
+          pixfmt_repr = WUFFS_BASE__PIXEL_FORMAT__RGBA_NONPREMUL;
+          break;
+        default:
+          // XR_LOGE("Number of channels is not supported");
+          return false;
+      }
+    }
+  }
+
+  wuffs_base__pixel_config__set(
+      &ic.pixcfg, pixfmt_repr, WUFFS_BASE__PIXEL_SUBSAMPLING__NONE, dim_x, dim_y);
+
+  switch (channels) {
+    case 1:
+      frame.init(ImageContentBlockSpec(PixelFormat::GREY8, dim_x, dim_y));
+      break;
+    case 3:
+      frame.init(ImageContentBlockSpec(PixelFormat::RGB8, dim_x, dim_y));
+      break;
+    case 4:
+      frame.init(ImageContentBlockSpec(PixelFormat::RGBA8, dim_x, dim_y));
+      break;
+    default:
+      XR_LOGE("PNG pixel format not supported");
+      return false;
+  }
+  uint8_t* dst_ptr = frame.wdata();
+
+  size_t workbuf_len = wuffs_png__decoder__workbuf_len(&dec).max_incl;
+  if (workbuf_len > SIZE_MAX) {
+    XR_LOGE("Invalid png! workbuf_len > SIZE_MAX");
+    return false;
+  }
+
+  vector<uint8_t> workbuf_slice_buffer(workbuf_len);
+  wuffs_base__slice_u8 workbuf_slice =
+      wuffs_base__make_slice_u8(workbuf_slice_buffer.data(), workbuf_len);
+
+  wuffs_base__slice_u8 pixbuf_slice = wuffs_base__make_slice_u8(dst_ptr, num_pixels * channels);
+  if (!pixbuf_slice.ptr) {
+    XR_LOGE("Failed to create pixbuf_slice");
+    return false;
+  }
+
+  wuffs_base__pixel_buffer pb;
+  status = wuffs_base__pixel_buffer__set_from_slice(&pb, &ic.pixcfg, pixbuf_slice);
+  if (status.repr) {
+    XR_LOGE("Failed to setup pixel_buffer");
+    return false;
+  }
+
+  while (true) {
+    wuffs_base__frame_config fc;
+    status = wuffs_png__decoder__decode_frame_config(&dec, &fc, &src);
+    if (status.repr == wuffs_base__note__end_of_data) {
+      break;
+    }
+    status = wuffs_png__decoder__decode_frame(
+        &dec, &pb, &src, WUFFS_BASE__PIXEL_BLEND__SRC, workbuf_slice, nullptr);
+    if (status.repr) {
+      XR_LOGE("Failed to decode png!");
+      return false;
+    }
+  }
+
+  return true;
+}
+
+#endif
+
 struct SourceBuffer {
   explicit SourceBuffer(const vector<uint8_t>& abuffer) : buffer{abuffer} {}
   const vector<uint8_t>& buffer;
   size_t readSize = 0;
 };
-
-static void pngStreamRead(png_structp pngPtr, png_bytep data, png_size_t length) {
-  SourceBuffer* src = (SourceBuffer*)png_get_io_ptr(pngPtr);
-  if (XR_VERIFY(src->readSize + length <= src->buffer.size())) {
-    memcpy(data, src->buffer.data() + src->readSize, length);
-    src->readSize += length;
-  } else {
-    memset(data, 0, length);
-  }
-}
 
 bool PixelFrame::readPngFrame(RecordReader* reader, uint32_t sizeBytes) {
   if (sizeBytes < kPngSigBytes) {
@@ -64,7 +272,22 @@ bool PixelFrame::readPngFrame(RecordReader* reader, uint32_t sizeBytes) {
   return readPngFrame(buffer);
 }
 
+static void pngStreamRead(png_structp pngPtr, png_bytep data, png_size_t length) {
+  SourceBuffer* src = (SourceBuffer*)png_get_io_ptr(pngPtr);
+  if (XR_VERIFY(src->readSize + length <= src->buffer.size())) {
+    memcpy(data, src->buffer.data() + src->readSize, length);
+    src->readSize += length;
+  } else {
+    memset(data, 0, length);
+  }
+}
+
 bool PixelFrame::readPngFrame(const vector<uint8_t>& pngBuffer, bool decodePixels) {
+#ifdef USE_WUFFS
+  if (decodePixels && readPngFrameWithWuff(*this, pngBuffer)) {
+    return true;
+  }
+#endif
   // Let LibPNG check the sig.
   SourceBuffer src(pngBuffer);
   if (png_sig_cmp(src.buffer.data(), 0, kPngSigBytes) != 0) {
