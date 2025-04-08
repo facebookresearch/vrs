184d69be28d24098bd960d355647a473f11ca84e
Pierre Moulon <pierrem@meta.com>
2025-04-08 10:36:58 -0700

    {Build} Add CMake support for OPUS build (#172)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/vrs/pull/172
    
    Auto-detect Opus if installed
    - Configure a CMake preprocessor `BUILD_WITH_OPUS` accordingly
    - If opus detected:
      - link Opus::opus to the target
      - add compilation preprocessor`OPUS_IS_AVAILABLE` CPP
    
    Differential Revision: D68632323

--- cmake/LibrariesSetup.cmake
+++ cmake/LibrariesSetup.cmake
@@ -23,6 +23,7 @@ find_package(Boost REQUIRED
     date_time
     system
 )
+
 find_package(Threads REQUIRED)
 find_package(FmtLib REQUIRED)
 find_package(RapidjsonLib REQUIRED)
@@ -32,6 +33,10 @@ find_package(xxHash REQUIRED)
 find_package(PNG REQUIRED)
 find_package(JPEG REQUIRED)
 find_package(TurboJpeg REQUIRED)
+
+# Optional dependencies
+find_package(Opus)
+
 
 # Setup unit test infra, but only if unit tests are enabled
 if (UNIT_TESTS)
