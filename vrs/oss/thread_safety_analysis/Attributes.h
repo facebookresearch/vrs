30fb6889cddf088f9b24e51a4b64ac7e2fc15926
Georges Berenger <gberenger@meta.com>
2026-06-16 17:52:41 -0700

    thread safety analysis (TSA) annotations for open source builds
    Summary: Empty implementation of TSA for OSS builds.
    
    Reviewed By: r-barnes
    
    Differential Revision: D108811525
    
    fbshipit-source-id: a93e676a34ee7d2ddae60e597fb51aab97590531

---
+++ vrs/oss/thread_safety_analysis/Attributes.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#define XR_TSA_CAPABILITY(x)
+#define XR_TSA_SCOPED_CAPABILITY
+#define XR_TSA_GUARDED_BY(x)
+#define XR_TSA_PT_GUARDED_BY(x)
+#define XR_TSA_ACQUIRED_BEFORE(...)
+#define XR_TSA_ACQUIRED_AFTER(...)
+#define XR_TSA_REQUIRES(...)
+#define XR_TSA_REQUIRES_SHARED(...)
+#define XR_TSA_ACQUIRE(...)
+#define XR_TSA_ACQUIRE_SHARED(...)
+#define XR_TSA_RELEASE(...)
+#define XR_TSA_RELEASE_SHARED(...)
+#define XR_TSA_RELEASE_GENERIC(...)
+#define XR_TSA_TRY_ACQUIRE(...)
+#define XR_TSA_TRY_ACQUIRE_SHARED(...)
+#define XR_TSA_EXCLUDES_DO_NOT_USE(...)
+#define XR_TSA_ASSERT_CAPABILITY(x)
+#define XR_TSA_ASSERT_SHARED_CAPABILITY(x)
+#define XR_TSA_RETURN_CAPABILITY(x)
+#define XR_TSA_NO_THREAD_SAFETY_ANALYSIS
+#define XR_TSA_GUARDED_BY_DSP_COMPATIBLE(x)
+#define XR_TSA_ACQUIRED_BEFORE_DSP_COMPATIBLE(...)
+#define XR_TSA_ACQUIRED_AFTER_DSP_COMPATIBLE(...)
+#define XR_TSA_REQUIRES_DSP_COMPATIBLE(...)
---
+++ vrs/oss/thread_safety_analysis/Mutex.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <mutex>
+#include <shared_mutex>
+
+#include "Attributes.h"
+
+namespace tsa {
+
+using mutex = std::mutex;
+using shared_mutex = std::shared_mutex;
+using recursive_mutex = std::recursive_mutex;
+using timed_mutex = std::timed_mutex;
+
+template <typename Mutex>
+using lock_guard = std::lock_guard<Mutex>;
+template <typename Mutex>
+using shared_lock = std::shared_lock<Mutex>;
+template <typename Mutex>
+using unique_lock = std::unique_lock<Mutex>;
+
+using std::adopt_lock;
+using std::adopt_lock_t;
+using std::defer_lock;
+using std::defer_lock_t;
+
+} // namespace tsa
